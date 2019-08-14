e32b54d8f9f50ecb16230a16d6daac3e75a4108d
fauen <daniel@backman.io>
2019-08-14 18:22:45 +0200

    first commit of the first script used for DigitalOcean machines

---
+++ first.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is basically the script I use when trying out everything
+# The point of it is setting up the start of a DigitalOcean machine
+
+useradd -mG sudo fauen
+passwd fauen
+mkdir /home/fauen/.ssh
+cp .ssh/authorized_keys /home/fauen/.ssh/
+chown -R fauen:fauen /home/fauen/.ssh/
+apt install git
