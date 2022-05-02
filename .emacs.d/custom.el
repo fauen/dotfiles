0184262de2be14c4d6da77d2b6de9c597ba88faf
fauen <daniel@backman.io>
2022-05-02 07:58:43 +0200

    updates

---
+++ .emacs.d/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("75b8719c741c6d7afa290e0bb394d809f0cc62045b93e1d66cd646907f8e6d43" default))
+ '(package-selected-packages
+   '(evil-collection evil doom-themes doom-modeline ivy-rich counsel which-key rainbow-delimiters use-package command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
