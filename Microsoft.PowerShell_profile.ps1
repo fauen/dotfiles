99b7f1508bd2424c2665333b7845a9aeb651702c
fauen <daniel@backman.io>
2023-02-11 13:45:19 +0100

    Default profile for PowerShell

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,29 @@
+# Set the prompt
+function prompt {"$env:USERNAME`@$env:COMPUTERNAME`:$(get-location) >>> "}
+
+# Test function to learn how it works and how Invoke-Webrequest can get correct content like curl.
+function weather {
+    if ( $args -eq $null ) 
+    {
+        (Invoke-WebRequest "wttr.in/?m").Content
+    }
+    else
+    {
+        (Invoke-WebRequest "wttr.in/${args}?m").Content
+    } 
+}
+
+# Make sure you have a font installed like Caskydia NF or you might get an error.
+if ( get-module -Name Terminal-Icons )
+{
+	import-module Terminal-Icons
+}
+else
+{
+	install-module -Name Terminal-Icons
+	import-module -Name Terminal-Icons
+}
+
+# Instead of using the default one, this will show a list.
+# You can also run Set-PSReadLineOption -PredictionSource None to not get any predictions.
+Set-PSReadLineOption -PredictionViewStyle ListView
