52588f4e4621fa0b0b8310161f8acb8b3a7654b5
fauen <daniel@backman.io>
2023-02-14 11:40:19 +0100

    Added a sourcing element.
    This will source all the function I use.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,13 @@
 # Set the prompt
 function prompt {"$env:USERNAME`@$env:COMPUTERNAME`:$(get-location) >>> "}
 
+if ($PSVersionTable.Platform -eq "Unix") {
+    . $env:HOME/PowershellFunctions.ps1
+}
+else {
+    . $env:HOMEPATH\PowershellFunctions.ps1
+}
+
 # Test function to learn how it works and how Invoke-Webrequest can get correct content like curl.
 function weather {
     if ( $args -eq $null ) 
