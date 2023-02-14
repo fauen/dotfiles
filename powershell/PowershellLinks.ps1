4ced3d09a15eaa1ca60292e13e19b88e65df81c4
Daniel Bäckman <daniel.backman@Daniels-MacBook-Pro.local>
2023-02-14 20:41:39 +0100

    New file added!
    Trying to make a dedicated file that creates links to my configuration files for PowerShell.

---
+++ powershell/PowershellLinks.ps1
@@ -0,0 +1,24 @@
+# Time to link some stuff.
+
+if ($PSVersionTable.Platform -eq "Unix") {
+    if (-not (Test-Path -Path $env:HOME/.config/powershell)) {
+        mkdir $env:HOME/.config/powershell
+        Write-Verbose "Directory has been created."
+    }
+    else {
+        Write-Verbose "Directory already exists."
+    }
+    ln -s $env:HOME/github/dotfiles/powershell/Microsoft.PowerShell_profile.ps1 $env:HOME/.config/powershell/$1
+    ln -s $env:HOME/github/dotfiles/powershell/PowershellPrompt.ps1 $env:HOME/.config/powershell/$1
+    ln -s $env:HOME/github/dotfiles/powershell/PowershellFunctions.ps1 $env:HOME/.config/powershell/$1
+    ln -s $env:HOME/github/dotfiles/powershell/PowershellAliases.ps1 $env:HOME/.config/powershell/$1
+}
+else {
+    if (-not (Test-Path -Path $env:HOMEPATH\.config\powershell)) {
+        mkdir $env:HOMEPATH\.config\powershell
+        Write-Verbose "Directory has been created."
+    }
+    else {
+        Start-Process "pwsh" -Verb RunAs -ArgumentList "-file C:\PS\Scripts\myPSScript.ps1"
+    }
+}
