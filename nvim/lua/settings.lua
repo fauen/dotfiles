d507586e78907f25215ca8548e1609e4af51912a
fauen <daniel@backman.io>
2025-01-14 09:14:13 +0100

    Some minor updates kinda.

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -14,3 +14,21 @@ vim.opt.cursorline = true	-- Highlight the current line
 vim.g.mapleader = ' '  -- Set leader key
 vim.keymap.set('n', '<leader>w', ':w<CR>', { silent = true }) -- Save file
 vim.keymap.set('n', '<leader>q', ':q<CR>', { silent = true }) -- Quit
+
+-- Do it right
+vim.keymap.set('n', '<Left>', ':echoe "Use H"<CR>', { silent = false })
+vim.keymap.set('n', '<Down>', ':echoe "Use J"<CR>', { silent = false })
+vim.keymap.set('n', '<Up>', ':echoe "Use K"<CR>', { silent = false })
+vim.keymap.set('n', '<Right>', ':echoe "Use L"<CR>', { silent = false })
+
+-- Window, tabs and splits
+-- vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true })
+-- vim.keymap.set('n', '<C-j>', '<C-w>j', { silent = true })
+-- vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
+-- vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
+
+-- vim.keymap.set('n', '<C-n>c', ':tabnew<CR>')
+-- vim.keymap.set('n', '<C-w><C-l>', ':tabnext<CR>')
+-- vim.keymap.set('n', '<C-w><C-h>', ':tabprevious<CR>')
+-- vim.keymap.set('n', '<C-w>-', ':split<CR>')
+-- vim.keymap.set('n', '<C-w>_', ':vertical split<CR>')
