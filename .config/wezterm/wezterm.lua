5b65f40fc0ec6b63d64dc8d5b3b74a58a2a6f617
Daniel Bäckman <daniel@backman.io>
2026-08-12 13:09:47 +0200

    Added keybinds for wezterm.
    Added panes and movement between them.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -42,5 +42,39 @@ config.hide_tab_bar_if_only_one_tab = true
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = true
 
+-- Keybinds
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+	{
+		key = '-',
+		mods = 'LEADER',
+		action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+	},
+	{
+		key = '_',
+		mods = 'LEADER',
+		action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+	},
+	{
+		key = 'h',
+		mods = 'LEADER',
+		action = wezterm.action.ActivatePaneDirection 'Left',
+	},
+	{
+		key = 'j',
+		mods = 'LEADER',
+		action = wezterm.action.ActivatePaneDirection 'Down',
+	},
+	{
+		key = 'k',
+		mods = 'LEADER',
+		action = wezterm.action.ActivatePaneDirection 'Up',
+	},
+	{
+		key = 'l',
+		mods = 'LEADER',
+		action = wezterm.action.ActivatePaneDirection 'Right',
+	},
+}
 -- and finally, return the configuration to wezterm
 return config
